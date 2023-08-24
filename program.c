78c65d5d3c99618625d84b16d08cfcde031265e3
mcdua <caicastro.gh@gmail.com>
2023-08-23 22:45:43 -0700

    update on program.c

--- program.c
+++ program.c
@@ -1,28 +1,28 @@
 #include <sys/wait.h>
-#include "shell.h"
 #include <unistd.h>
+#include "shell.h"
 
 #define MAX_COMMAND 20
 
 /**
- * program - A simple cmd line shell
- * @av: Takes arguments
- * @env: display env var
+ * program - A simple command line shell
+ * @av: Arguments passed to the program
+ * @env: Environment variables to display
  */
-
 void program(char **av, char **env)
 {
 	char *pull = NULL;
-	int x, w, pos;
+	int x, w;
 	size_t y = 0;
 	ssize_t no_ch;
 	char *fly[MAX_COMMAND];
 	pid_t baby_pid;
+	int pos;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("cisfun$ ");
+			write(STDOUT_FILENO, "cisfun$ ", 8);
 
 		no_ch = getline(&pull, &y, stdin);
 		if (no_ch == -1)
@@ -30,6 +30,7 @@ void program(char **av, char **env)
 			free(pull);
 			exit(0);
 		}
+
 		x = 0;
 		while (pull[x])
 		{
@@ -37,6 +38,7 @@ void program(char **av, char **env)
 				pull[x] = 0;
 			x++;
 		}
+
 		w = 0;
 		fly[0] = strtok(pull, " ");
 		while (fly[w] != NULL)
@@ -44,18 +46,22 @@ void program(char **av, char **env)
 			w++;
 			fly[w] = strtok(NULL, " ");
 		}
+
 		baby_pid = fork();
 		if (baby_pid == -1)
 		{
 			free(pull);
 			exit(0);
 		}
+
 		if (baby_pid == 0)
 		{
 			if (execve(fly[0], fly, env) == -1)
-				printf("%s: No such file or directory\n", av[0]);
+				write(STDOUT_FILENO, av[0], _strlen(av[0]));
+			exit(0);
 		}
 		else
 			wait(&pos);
+		free(pull);
 	}
 }
